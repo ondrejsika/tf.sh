7a40e27bafe0dd0843444ea221f1508092c9315c
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-31 13:41:48 +0100

    Add locking mechanism

--- tf.sh
+++ tf.sh
@@ -5,9 +5,20 @@ cd "$(dirname $0)"
 [ -f .tf.env.sh ] && \
   . .tf.env.sh
 
-git -C .state pull origin master && \
+git -C .state pull origin master
+
+if [ -f ".state/lock" ] ; then
+  echo "ERR: terraform state is locked" >&2
+  exit 1
+fi
+
+touch .state/lock && \
+  git -C .state add . && \
+  git -C .state commit -m "[terraform] lock state" $GIT_COMMIT_FLAGS && \
+  git -C .state push origin master && \
   terraform $@
 
-git -C .state add . && \
-  git -C .state commit -m "[terraform] update state" $GIT_COMMIT_FLAGS && \
+rm .state/lock && \
+  git -C .state add . && \
+  git -C .state commit -m "[terraform] update & unlock state" $GIT_COMMIT_FLAGS && \
   git -C .state push origin master
